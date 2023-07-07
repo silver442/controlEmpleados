e5f01752a9c46142f9a4ad34c007bf4dc92d99fa
Alexhoyosf <alexhoyosf080302@gmail.com>
2023-07-07 13:48:44 -0600

    Login funcionando

--- src/app/login-component/login-component.component.ts
+++ src/app/login-component/login-component.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login-component',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-component.component.css']
 })
 export class LoginComponentComponent {
+
+  constructor(private usrService:UserService, private router:Router){
+
+  }
+
+  doLogin(idUser:string, pin:string): void {
+    
+    var idusr:number = parseInt("0"+idUser);
+    var pinusr:number = parseInt("0"+pin);
+
+    this.usrService.login(idusr, pinusr).then((canLogIn) =>{
+
+      if(canLogIn)
+        this.router.navigate(["/bienvenida"])
+      else
+        alert("ID Usuario o PIN Invalido")
+
+    })
+
+  }
 
 }
