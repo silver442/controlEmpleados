33a8b68c1cdcc7b8471d06eaa5a03fae713a3127
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2023-07-08 18:55:37 -0600

    Se agrego input radio que mestra mensaje de salida en la pantalla de salida

--- src/app/salida-component/salida-component.component.ts
+++ src/app/salida-component/salida-component.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-salida-component',
   templateUrl: './salida-component.component.html',
   styleUrls: ['./salida-component.component.css']
 })
-export class SalidaComponentComponent {
+export class SalidaComponentComponent{
+  textSalida = "El empleado aun no sale"
+
+  setSalida(){
+    this.textSalida = "El empleado ha registrado su salida";
+  }
 
 }
+
+
+
